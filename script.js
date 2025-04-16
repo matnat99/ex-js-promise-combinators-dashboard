193a36472e5b487ed2813bc0ecf42877c8db1d65
Mattia Natale <mattianatale07@gmail.com>
2025-04-16 18:04:48 +0200

    feat: add bonus 1

--- script.js
+++ script.js
@@ -22,6 +22,13 @@ Stampare i dati in console in un messaggio ben formattato.
 Testa la funzione con la query "london"
 */
 
+/*
+Bonus 1 - Risultato vuoto
+Se l’array di ricerca è vuoto, invece di far fallire l'intera funzione, 
+semplicemente i dati relativi a quella chiamata verranno settati a null e  la frase relativa non viene stampata. 
+Testa la funzione con la query “vienna” (non trova il meteo).
+*/
+
 async function fetchJson(url) {
   const response = await fetch(url);
   const object = await response.json();
@@ -43,12 +50,16 @@ async function getDashboardData(query) {
     const promises = [destinationsPromise, weathersPromise, airportsPromise];
     const [destinations, weathers, airports] = await Promise.all(promises);
 
+    const destination = destinations[0];
+    const weather = weathers[0];
+    const airport = airports[0];
+
     return {
-      city: destinations[0].name,
-      country: destinations[0].country,
-      temperature: weathers[0].temperature,
-      weather: weathers[0].weather_description,
-      airport: airports[0].name,
+      city: destination ? destinations[0].name : null,
+      country: destination ? destinations[0].country : null,
+      temperature: weather ? weathers[0].temperature : null,
+      weather: weather ? weathers[0].weather_description : null,
+      airport: airport ? airports[0].name : null,
     };
   } catch (error) {
     throw new Error(`Errore recupero dati: ${error.message}`);
@@ -56,14 +67,20 @@ async function getDashboardData(query) {
 }
 
 (async () => {
-  getDashboardData("london")
+  getDashboardData("vienna")
     .then((data) => {
       console.log("Dasboard data:", data);
-      console.log(
-        `${data.city} is in ${data.country}.\n` +
-          `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n` +
-          `The main airport is ${data.airport}.\n`
-      );
+      let phrase = "";
+      if (data.city !== null && data.country !== null) {
+        phrase += `${data.city} is in ${data.country}.\n`;
+      }
+      if (data.temperature !== null && data.weather) {
+        phrase += `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`;
+      }
+      if (data.airport !== null) {
+        phrase += `The main airport is ${data.airport}.\n`;
+      }
+      console.log(phrase);
     })
     .catch((error) => console.error(error));
 })();
